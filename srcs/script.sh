df3a9496b327eafe342db86fb7452c05dc4ddd89
lspohle <121381385+lspohle@users.noreply.github.com>
2023-03-04 17:57:55 +0100

    Update

--- srcs/script.sh
+++ srcs/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "alias alv='cd ~/PRIVATE_a_little_valentine/srcs && make -s re'" >> ~/.zshrc
+echo "alias alv='cd ~/PRIVATE_a_little_valentine/srcs && ./a_little_valentine'" >> ~/.zshrc
 zsh
